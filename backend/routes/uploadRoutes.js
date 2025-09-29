5748bf4dc9b8c3e3a330a85a1bd0fd79e2f4873b
Rayhan <mandlirayhan@gmail.com>
2025-09-29 13:16:36 +0530

    vfs upload functionality with directory support

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -2,27 +2,30 @@ import express from "express";
 import { createWriteStream } from "fs";
 import path from "path";
 import filesData from "../db/fileDB.json" with { type: "json" };
+import directoriesData from "../db/directoryDB.json" with { type: "json" };
 import { writeFile } from "fs/promises";
 
 const router = express.Router();
 
 //Upload files
 router.post("/:filename", (req, res) => {
+  const parentDirID = req.headers["parentdirid"] || "root";
   let { filename } = req.params;
   const id = crypto.randomUUID();
-  console.log('id: ', id);
   const extension = path.extname(filename);
-  console.log('extension: ', extension);
   const fileNameWithID = id + extension;
-  console.log('fileNameWithID: ', fileNameWithID);
 
   const writeStream = createWriteStream(`./storage/${fileNameWithID}`);
   req.pipe(writeStream);
 
   writeStream.on("finish", async() => {
-    filesData.push({ id, filename, extension, pDir:0, createdAt: new Date().toISOString() });
+    const parentDir = directoriesData.find(dir => dir.id === parentDirID);
+    parentDir.files.push(id);
+    
+    filesData.push({ id, filename, extension, pDir:parentDirID, createdAt: new Date().toISOString() });
     try{
         await writeFile("./db/fileDB.json", JSON.stringify(filesData, null, 2));
+        await writeFile("./db/directoryDB.json", JSON.stringify(directoriesData, null, 2));
     }catch(e){
         return res.status(500).json({ message: "Error saving file metadata" });
     }
