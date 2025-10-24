b21f91f5b46164935ee74daf31bf34e7f802ac93
Rayhan <mandlirayhan@gmail.com>
2025-10-24 15:03:49 +0530

    fileRoutes updated

--- backend/routes/filesRoutes.js
+++ backend/routes/filesRoutes.js
@@ -1,18 +1,15 @@
 import express from "express";
-import { Db, ObjectId } from "mongodb";
+import { File } from "../models/fileModel.js";
 
 const router = express.Router();
 
-//Serve files
 router
   .route("/:id")
   .get(async (req, res, next) => {
     const { id } = req.params;
     const { action } = req.query;
-    const db = req.db;
-    const filesCollection = db.collection("files");
 
-    const file = await filesCollection.findOne({ _id: new ObjectId(id) });
+    const file = await File.findById(id);
 
     if (!file) return res.status(404).json({ message: "File not found" });
 
@@ -24,24 +21,22 @@ router
       res.sendFile(`${process.cwd()}/storage/${id}${file.extension}`);
     } catch (e) {
       res.status(400).json({ message: e.message });
+      next(e);
     }
   })
   .patch(async (req, res) => {
     const { id } = req.params;
-    const db = req.db;
-    const filesCollection = db.collection("files");
 
     let { newName } = req.body;
 
     try {
-      let file = await filesCollection.findOne({ _id: new ObjectId(id) });
-      if (!file) return res.status(404).json({ message: "File not found" });
-      const updatedFile = await filesCollection.updateOne(
-        { _id: file._id },
-        { $set: { filename: newName } }
+      let file = await File.findByIdAndUpdate(
+        { _id: id },
+        { filename: newName },
+        { new: true }
       );
-      if (updatedFile.modifiedCount === 0)
-        return res.status(500).json({ message: "Could not update file" });
+      if (!file) return res.status(404).json({ message: "File not found" });
+
       res.status(200).json({ message: "file renamed" });
     } catch (e) {
       next(e);
